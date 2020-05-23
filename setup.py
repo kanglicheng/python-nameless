b94b0fc0c785ab62fd4c7aff6ba28ce62e9e8d2a
Your Name <you@example.com>
2020-05-23 22:55:16 +0000

    Bump version: 1.0.0 → 1.658.18

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ class optional_build_ext(build_ext):
 
 setup(
     name='nameless',
-    version='1.0.0',
+    version='1.658.18',
     license='BSD-2-Clause',
     description='An example package. Generated with cookiecutter-pylibrary.',
     long_description='%s\n%s' % (
